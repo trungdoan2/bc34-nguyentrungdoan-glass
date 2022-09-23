21d96f96584660c09c77d6d4d08724af0d87a183
trungdoan2 <108280266+trungdoan2@users.noreply.github.com>
2022-09-23 22:28:48 +0700

    a

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+import './App.css';
+import DemoComponent from './component/demobuoimot/DemoComponent';
+import BaiTapLayOut from './component/demo_layout/BaiTapLayOut';
+import DemoState from './component/demoState/DemoState';
+import BaiTapCar from './component/bt_car_basic/BaiTapCar';
+import BaiTapMovie from './component/bt_car_basic/BaiTapMovie';
+import BaiTapThucHanhLayOut from './component/BaiTapLayOutComponent/BaiTapThucHanhLayOut';
+import DemoProps from './component/demo_props/DemoProps';
+import TrangChuSanPham from './component/bt_phone/TrangChuSanPham';
+import BaiTapGlass from './component/bt-glass/BaiTapGlass';
+
+function App() {
+  return (
+    <div className="App">
+       {/* <DemoComponent /> */}
+        {/* <BaiTapLayOut /> */}
+        {/* <DemoComponent /> */}
+        {/* <DemoState  /> */}
+        {/* <BaiTapCar /> */}
+        {/* <BaiTapMovie /> */}
+        {/* <BaiTapThucHanhLayOut /> */}
+        {/* <DemoProps /> */}
+        <BaiTapGlass />
+        {/* <TrangChuSanPham /> */}
+    </div>
+  );
+}
+
+export default App;
